235d7b27694b514c02d74dfaa674b7c6eaa66f53
SagarMhzn <maharzansagar4u@gmail.com>
2022-05-08 12:12:19 +0545

    lab1

--- Lexical Analyzer/lab 1.c
+++ Lexical Analyzer/lab 1.c
@@ -4,5 +4,28 @@
 
 int main()
 {
+	char token[5],currentState,startState='A',finalState='E';
+	printf("Enter a keyword: \n");
+	gets(token);
+	int i;
+	currentState = startState;
 	
+	for(i=0; token[i]!='\0';i++)
+	{
+		if ( token[i]=='i' && currentState == 'A')
+			currentState = 'B';
+		else if ( token[i]=='n' && currentState == 'B')
+			currentState = 'C';
+		else if ( token[i]=='t' && currentState == 'C')
+			currentState = 'E';
+		else
+			currentState = 'D';
+	}
+	
+	if (currentState == finalState)
+		printf("The entered string is a Valid Keyword!");
+	else 
+		printf("The entered string is not a Valid Keyword!");
+		
+	return 0;
 }
